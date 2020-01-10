8563f39685dfb34c2b29ed5b8064d4d46f26d1cb
ichad17 <chad.s.whitaker@gmail.com>
2020-01-10 19:43:51 +0000

    MITYO inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+var appname = 'CITYO';
+
+class List extends React.Component {
+  render() {
+    return (
+      <ul className="navList">
+        <li className="navListItem">Login</li>
+        <li className="navListItem">Username</li>
+      </ul>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h1 className ="App-title">{appname }</h1>
+        <List />
+      </div>
+    );
+  }
+}
+
+class Logginform extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = {value:'', value2:''};
+
+   this.changeHandler = this.changeHandler.bind(this);
+   this.changeHandler2 = this.changeHandler2.bind(this);
+ }
+
+ changeHandler(event) {
+
+  this.setState({value: event.target.value});
+
+}
+
+  changeHandler2(event) {
+    this.setState({value2: event.target.value});
+  }
+
+
+
+  render(){
+    return (
+      <div className="flex">
+        <form>
+          <label>
+            Username:
+            <input type="text" value={this.state.value} onChange={this.changeHandler} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={this.state.value2} onChange={this.changeHandler2} />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+class Container extends React.Component {
+  clickHandler() {
+    var stop = 10;
+    var test = this.clickHandler;
+    console.log(test);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Login to {appname}</h2>
+        <Logginform />
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Header />
+      </div>
+      <div className="mainbit">
+        <Container />
+      </div>
     </div>
   );
 }
